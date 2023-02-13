4e3aff5fb5877b80fd900ebb3f2888cbe2ab7ef8
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-02-13 11:46:29 +0600

    first c

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chat-ai-0ste.onrender.com', {
+    const response = await fetch('https://chat-ai-0ste.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
